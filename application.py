fdde43360af671c4183170cb4949bb5064b04713
何玉莲 <alina.he@ixiaoshuidi.com>
2019-01-11 21:49:19 +0800

    request and response

--- application.py
+++ application.py
@@ -6,6 +6,8 @@ import config
 class Application(tornado.web.Application):
     def __init__(self):
         handlers = [
-            (r'/', index.IndexHandler)
+            (r'/', index.IndexHandler),
+            (r'/param_transmit/(\w+)/(\w+)/(\w+)', index.ParamURIHandler),
+            (r'/get_method', index.GetMethodHandler),
         ]
         super(Application, self).__init__(handlers, **config.options)
--- views/index.py
+++ views/index.py
@@ -11,3 +11,33 @@ class IndexHandler(RequestHandler):
     def post(self, *args, **kwargs):
         pass
 
+
+class ParamURIHandler(RequestHandler):
+    '''
+    类比diango中的视图
+    当请求的URI为 r'/param_transmit/p1/p2/p3'的时候
+    '''
+    def get(self, param1, param2, param3, *args, **kwargs):
+        print('debug ParamURIHandler params:', param1, param2, param3)
+        self.write('hello tornado paramURIHandler!')
+
+    def post(self, *args, **kwargs):
+        pass
+
+
+class GetMethodHandler(RequestHandler):
+    '''
+    类比diango中的视图
+    当请求的URI为 r'/get_method?a=1&b=2&c=3'的时候
+    '''
+    def get(self, *args, **kwargs):
+        a = self.get_query_argument('a')
+        b = self.get_query_argument('b')
+        c = self.get_query_argument('c')
+        # 获取同名参数值的列表
+        d = self.get_query_arguments('c')
+        print('debug GetMethodHandler params:', a, b, c)
+        self.write('hello tornado GetMethodHandler!')
+
+    def post(self, *args, **kwargs):
+        pass
